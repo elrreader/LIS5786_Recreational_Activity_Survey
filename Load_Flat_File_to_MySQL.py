097d9423b7bbe952f6e745ed7b955256e37305a9
Elissa_Reiskind <elissa.reiskind@gmail.com>
2021-09-14 22:06:05 -0400

    Create Load_Flat_File_to_MySQL script

---
+++ Load_Flat_File_to_MySQL.py
@@ -0,0 +1,10 @@
+
+def main():
+    """Takes in the survey response flat file and loads the data into a MySQL database.
+
+    This command line script ingests the `Recreational Activity Survey (Responses)` flat file provided by Google Forms into a pandas dataframe, separates the fields given as comma seperated lists into their own dataframes, and loads all three dataframes into a MySQL database. 
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
