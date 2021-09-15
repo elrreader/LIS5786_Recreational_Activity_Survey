30bab0ad540d7f61051729368ded8112500ee60b
Elissa_Reiskind <elissa.reiskind@gmail.com>
2021-09-14 23:02:21 -0400

    Load CSV into dataframe

--- Load_Flat_File_to_MySQL.py
+++ Load_Flat_File_to_MySQL.py
@@ -1,10 +1,26 @@
+import logging
+import os
+import sys
+import pandas as pd
+
 
 def main():
     """Takes in the survey response flat file and loads the data into a MySQL database.
 
     This command line script ingests the `Recreational Activity Survey (Responses)` flat file provided by Google Forms into a pandas dataframe, separates the fields given as comma seperated lists into their own dataframes, and loads all three dataframes into a MySQL database. 
     """
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
+
     #Section: Create Dataframe from CSV File
+    CSV_file_location = input("Please provide the absolute file path to the CSV with the survey response data. The path can be copied from the file explorer by selecting the file, then dragging and dropping it into this terminal window. ")
+    if CSV_file_location[0] == '"' and CSV_file_location[-1] == '"':  # If the file path has spaces in it, it will be enclosed in quotes, making it an invalid file path; this removes the quotes if present
+        CSV_file_location = CSV_file_location[1:-1]
+    try:
+        CSV_dataframe = pd.read_csv(CSV_file_location)
+        logging.info(f"The CSV as a dataframe:\n{CSV_dataframe.head()}")
+    except:
+        logging.info("The file path provided is invalid. The program is exiting; use the up arrow to get the last terminal command if you wish to rerun this program.")
+        sys.exit()
 
 
     #Section: Create Dataframe to Load into MySQL
