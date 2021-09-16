f2bb01f3974323881efd7474ccb9536045e27ab7
Elissa_Reiskind <elissa.reiskind@gmail.com>
2021-09-16 13:33:11 -0400

    List out desired persona creation queries

---
+++ Persona_Creation_Queries.sql
@@ -0,0 +1,20 @@
+-- CROSSTABS --
+-- Crosstab of `Time_of_Day` and `Max_Distance`: Does when students want to work out impact how far from campus they want to travel?
+
+-- Crosstab of `Time_of_Day` and `Workout_Location`: Is there a relationship between when and where people want to work out?
+
+-- Crosstab of `Number_of_People` and `Max_Price`: Are people who want smaller classes willing to pay more?
+
+-- Crosstab of `Workout_Location` and `Max_Distance`: Does the desired workout location set how far people are willing to travel?
+
+-- Crosstab of `Number_of_People` and `Workout_Location`: Is working out further from campus seen as a way to meet people?
+
+
+-- PERCENT SELECTED --
+-- Percent selecting given `Workout_Location` selecting each `Activity`: How strong is the relationship between preferred workout location and activity?
+
+-- Percent selecting given `Max_Distance` selecting each `Day_of_Week`: Does what day of the week students want to work out impact how far from campus they want to travel?
+
+-- Percent selecting given `Time_of_Day` selecting each `Day_of_Week`: Is the preferred workout time consistant across all days of the week?
+
+-- Precent selecting given `Number_of_People` selecting each `Activity`: What relationship is there between group sports and preferences for larger groups?
