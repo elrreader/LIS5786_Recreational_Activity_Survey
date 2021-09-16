ce461e197815a21a23aa914f5dda5b5e84460abc
Elissa_Reiskind <elissa.reiskind@gmail.com>
2021-09-16 13:00:22 -0400

    Create initial join queries

--- Queries.sql
+++ Queries.sql
@@ -12,3 +12,47 @@ SELECT
     COUNT(Activity)
 FROM Activities
 GROUP BY Activity;
+
+
+-- The number of respondents selecting each option in the multiple-selection field `Activity`
+SELECT
+    COUNT(Survey_Answers.Survey_Answer_ID) AS Participants_Selecting,
+    Activities.Activity
+FROM
+    Survey_Answers,
+    Activities
+WHERE Survey_Answers.Survey_Answer_ID = Activities.Survey_Answer_ID
+GROUP BY Activities.Activity;
+
+
+-- The total number of responses to the multiple-selection field `Activity` selected by the respondents who picked each of the options from the single-selection field `Workout_Location`
+SELECT
+	Survey_Answers.Workout_Location,
+    COUNT(Activities.Activity) AS Number_of_Activities_Selected
+FROM Survey_Answers
+	JOIN Activities ON Survey_Answers.Survey_Answer_ID = Activities.Survey_Answer_ID
+GROUP BY Survey_Answers.Workout_Location;
+
+
+-- The number of respondents who selected each option from the single-selection field `Workout_Location` who selected the given response to the multiple-selection field `Activity`
+SELECT
+	Survey_Answers.Workout_Location,
+    Activities.Activity,
+    COUNT(Activities.Activity) AS Participants_Selecting
+FROM Survey_Answers
+	JOIN Activities ON Survey_Answers.Survey_Answer_ID = Activities.Survey_Answer_ID
+GROUP BY
+    Survey_Answers.Workout_Location,
+    Activities.Activity;
+
+SELECT
+    Survey_Answers.Workout_Location,
+    Activities.Activity,
+    COUNT(Survey_Answers.Workout_Location) AS Participants_Selecting
+FROM
+    Survey_Answers,
+    Activities
+WHERE Survey_Answers.Survey_Answer_ID = Activities.Survey_Answer_ID
+GROUP BY
+    Survey_Answers.Workout_Location,
+    Activities.Activity;
