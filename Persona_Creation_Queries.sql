a95ee46519af7899bda4e107c96fcacc3f4e1678
Elissa_Reiskind <elissa.reiskind@gmail.com>
2021-09-16 13:40:22 -0400

    Create crosstab queries

--- Persona_Creation_Queries.sql
+++ Persona_Creation_Queries.sql
@@ -1,13 +1,58 @@
 -- CROSSTABS --
 -- Crosstab of `Time_of_Day` and `Max_Distance`: Does when students want to work out impact how far from campus they want to travel?
+SELECT
+    Time_of_Day,
+    Max_Distance,
+    COUNT(*) AS Participants_Selecting
+FROM Survey_Answers
+GROUP BY
+    Time_of_Day,
+    Max_Distance
+ORDER BY Participants_Selecting DESC;
 
 -- Crosstab of `Time_of_Day` and `Workout_Location`: Is there a relationship between when and where people want to work out?
+SELECT
+    Time_of_Day,
+    Workout_Location,
+    COUNT(*) AS Participants_Selecting
+FROM Survey_Answers
+GROUP BY
+    Time_of_Day,
+    Workout_Location
+ORDER BY Participants_Selecting DESC;
 
 -- Crosstab of `Number_of_People` and `Max_Price`: Are people who want smaller classes willing to pay more?
+SELECT
+    Number_of_People,
+    Max_Price,
+    COUNT(*) AS Participants_Selecting
+FROM Survey_Answers
+GROUP BY
+    Number_of_People,
+    Max_Price
+ORDER BY Participants_Selecting DESC;
 
 -- Crosstab of `Workout_Location` and `Max_Distance`: Does the desired workout location set how far people are willing to travel?
+SELECT
+    Workout_Location,
+    Max_Distance,
+    COUNT(*) AS Participants_Selecting
+FROM Survey_Answers
+GROUP BY
+    Workout_Location,
+    Max_Distance
+ORDER BY Participants_Selecting DESC;
 
 -- Crosstab of `Number_of_People` and `Workout_Location`: Is working out further from campus seen as a way to meet people?
+SELECT
+    Number_of_People,
+    Workout_Location,
+    COUNT(*) AS Participants_Selecting
+FROM Survey_Answers
+GROUP BY
+    Number_of_People,
+    Workout_Location
+ORDER BY Participants_Selecting DESC;
 
 
 -- PERCENT SELECTED --
